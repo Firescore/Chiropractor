2128933a7348f6652a495509df35b629f7004eed
dhiraj7894 <dhiraj7894@gmail.com>
2020-08-18 16:31:15 +0530

    Google.com

--- Assets/levelManager.cs
+++ Assets/levelManager.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class levelManager : MonoBehaviour
 {
     public GameObject[] scene;
+    public GameObject mainMenu,LevelUI,playButton, nextButton, retryButton;
     public TextMeshProUGUI levelText;
     public int level = 0, i, i1;
     // Start is called before the first frame update
     void Start()
     {
+        LevelUI.SetActive(false);
+        playButton.SetActive(true);
+        /*
         i1 = 0;
         i = 0;
         scene[i].SetActive(true);
         level += 1;
-        levelText.text = "Level " + level.ToString();
+        levelText.text = "Level " + level.ToString();*/
     }
 
     // Update is called once per frame
@@ -41,5 +44,34 @@ public class levelManager : MonoBehaviour
     {
         scene[i].SetActive(true);
         scene[i1].SetActive(false);
+    }
+    public void Play()
+    {
+        LevelUI.SetActive(true);
+        playButton.SetActive(false);
+        mainMenu.SetActive(false);
+        i1 = 0;
+        i = 0;
+        scene[i].SetActive(true);
+        level += 1;
+        levelText.text = "Level " + level.ToString();
+    }
+    public void nexLevel()
+    {
+        if (i <= 4)
+        {
+                if (i >= 1)
+                {
+                    i1 += 1;
+                }
+                i += 1;
+                sceneEnable();
+                level += 1;
+                levelText.text = "Level " + level.ToString();
+        }
+    }
+    public void retry()
+    {
+        sceneEnable();
     }
 }
